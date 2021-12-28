206a28bfc565fc1f4e4d910fa590c7dc1f5db568
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-12-28 11:33:19 -0800

    Update Kernel.cs

--- Source/Kernel.cs
+++ Source/Kernel.cs
@@ -1,20 +1,24 @@
 using System;
 using Sys = Cosmos.System;
-
+using Cosmos.System.FileSystem.VFS;
 
 namespace Shard
 {
     public class Kernel : Sys.Kernel
     {
+        Sys.FileSystem.CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
         protected override void BeforeRun()
         {
             Console.WriteLine("ShardOS booted successfully. Type 'help' for a list of commands");
-            Console.WriteLine("Booting into Shell\n")
+            Console.WriteLine("Booting into Shell\n");
+            Sys.FileSystem.CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+            fs.Format("0" /*drive id*/, "FAT32" /*fat type*/, true /*use quick format*/);
         }
 
         protected override void Run()
         {
-            string version = "0.1.0-02";
+            string version = "0.2.0";
             string input;
             Console.Write("/> ");
             input = Console.ReadLine();
@@ -25,7 +29,15 @@ namespace Shard
             }
             if (input == "help")
             {
-                Console.WriteLine("\nCommands Availible:\nHelp --- Gets you to this message\nAbout --- Tells you about this OS");
+                Console.WriteLine("\nCommands Availible:\nHelp --- Gets you to this message\nAbout --- Tells you about this OS\nGetSize --- Tells you the fs size\nGetType --- Tells you the fs type");
+            }
+            if (input == "GetSize") {
+                var available_space = fs.GetAvailableFreeSpace(@"0:\");
+                Console.WriteLine("Available Free Space: " + available_space);
+            }
+            if (input == "GetType") {
+                var fs_type = fs.GetFileSystemType(@"0:\");
+                Console.WriteLine("File System Type: " + fs_type);
             }
         }
     }
