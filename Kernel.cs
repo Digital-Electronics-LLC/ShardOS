58ccada64d64f5f83e474f6800fbb53f05c6b0ce
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-12-28 09:52:30 -0800

    Add files via upload

---
+++ Kernel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Sys = Cosmos.System;
+
+namespace Shard
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("ShardOS booted successfully. Type 'help' for a list of commands");
+            Console.WriteLine("Booting into Shell\n");
+        }
+
+        protected override void Run()
+        {
+            string input;
+            Console.WriteLine("Command:");
+            input = Console.ReadLine();
+
+            if (input == "about") 
+            {
+                Console.WriteLine("\nThis is ShardOS 0.1.0\nYou are using a dev version\nPlease update as soon as possible\n");
+            }
+            if (input == "help")
+            {
+                Console.WriteLine("\nCommands Availible:\nHelp --- Gets you to this message\nAbout --- Tells you about this OS");
+            }
+        }
+    }
+}
