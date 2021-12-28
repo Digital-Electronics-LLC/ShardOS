362e188f1a20cb9a084a4a2d70e7dd85aa1f5a22
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-12-28 12:59:42 -0800

    Update Kernel.cs

--- Source/Kernel.cs
+++ Source/Kernel.cs
@@ -1,6 +1,7 @@
 using System;
 using Sys = Cosmos.System;
 using Cosmos.System.FileSystem.VFS;
+using System.IO;
 
 namespace Shard
 {
@@ -23,7 +24,7 @@ namespace Shard
             Console.Write("/> ");
             input = Console.ReadLine();
 
-            if (input == "about") 
+            if (input == "about")
             {
                 Console.WriteLine("\nThis is ShardOS", version, "\nYou are using a dev version\nPlease update as soon as possible\n");
             }
@@ -31,25 +32,29 @@ namespace Shard
             {
                 Console.WriteLine("\nCommands Availible:\nHelp --- Gets you to this message\nAbout --- Tells you about this OS\nGetSize --- Tells you the fs size\nGetType --- Tells you the fs type");
             }
-            if (input == "GetSize") {
+            if (input == "GetSize")
+            {
                 var available_space = fs.GetAvailableFreeSpace(@"0:\");
                 Console.WriteLine("Available Free Space: " + available_space);
             }
-            if (input == "GetType") {
+            if (input == "GetType")
+            {
                 var fs_type = fs.GetFileSystemType(@"0:\");
                 Console.WriteLine("File System Type: " + fs_type);
             }
-            if (input == "ls") {
+            if (input == "ls")
+            {
                 var directory_list = Directory.GetFiles(@"0:\");
                 foreach (var file in directory_list)
                 {
                     Console.WriteLine(file);
                 }
             }
-            if (input == "mk") {
+            if (input == "mk")
+            {
                 Console.Write("File name: ");
                 string fname = Console.ReadLine();
-                
+
                 try
                 {
                     var file_stream = File.Create(@"0:\" + fname);
@@ -60,7 +65,8 @@ namespace Shard
                 }
                 Console.WriteLine("File successfully created");
             }
-            if (input == "wr") {
+            if (input == "wr")
+            {
                 Console.WriteLine("Which File would you like to write to? Type the name and extension ex: example.txt");
                 string file = Console.ReadLine();
                 Console.WriteLine("What would you like to write into this file? Type the contents ex: hello! My name is John");
@@ -73,7 +79,7 @@ namespace Shard
                 {
                     Console.WriteLine(e.ToString());
                 }
-           } 
+            }
         }
     }
 }
